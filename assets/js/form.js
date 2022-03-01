6f1ce6cae0d96b456e7faf65ddd1394ac4dd74c0
Anna Greaves <gilhespy.anna@gmail.com>
2022-03-01 07:30:00 +0100

    Add remaining form fields and contentyTypes obj

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,58 @@
-console.log("works");
+// Data
+
+let contentTypes = [{
+    name: "textBasedUnit",
+    glhWeight: 1,
+    inputID: "textUnitInput",
+    glhOutputID: "textUnitOutput",
+},
+{
+    name: "textChallenge",
+    glhWeight: 10,
+    inputID: "textChallengeInput",
+    glhOutputID: "textChallengeOutput",
+},
+{
+    name: "quiz",
+    glhWeight: 1,
+    inputID: "quizUnitInput",
+    glhOutputID: "quizUnitOutput",
+},
+{
+    name: "passiveVideo",
+    glhWeight: 1.2,
+    inputID: "passiveVideoInput",
+    glhOutputID: "passiveVideoOutput",
+},
+{
+    name: "operationalVideo",
+    glhWeight: 3,
+    inputID: "operationalVideoInput",
+    glhOutputID: "operationalVideoOutput",
+},
+{
+    name: "activeVideo",
+    glhWeight: 8,
+    inputID: "activeVideoInput",
+    glhOutputID: "activeVideoOutput",
+},
+{
+    name: "intensiveVideo",
+    glhWeight: 11,
+    inputID: "intensiveVideoInput",
+    glhOutputID: "intensiveVideoOutput",
+},
+]
+
+/**
+ * Handle all submit functionality
+ * @param {obj} form 
+ */
+function handleSubmit(form) {
+    
+
+}
+
+function calculateGLH(contentType) {
+
+}
