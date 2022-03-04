c207209b1bb0af4c1d1b6f669869ab6d44dffc29
Anna Greaves <gilhespy.anna@gmail.com>
2022-03-04 21:10:23 +0100

    Add calculations and style results.

--- assets/js/form.js
+++ assets/js/form.js
@@ -58,9 +58,51 @@ function handleSubmit(form) {
     let avgVideoLength = document.getElementById("avgVideoLength").value;
     let contentLevel = document.getElementById("contentLevelInput").value;
 
-    let totalGLH = calculateGlhPerType(contentTypes, avgVideoLength, contentLevel);
+    let totalGLH = calculateGLHPerType(contentTypes, avgVideoLength, contentLevel);
+    let totalNumUnits = getTotalNumUnits(contentTypes);
+    let totalHoursToComplete = calculateTotalHours(form, totalNumUnits);
+    let totalDaysToComplete = totalHoursToComplete / 7.5; // 7.5 hours in a work day
+    let totalWeeksToComplete = totalDaysToComplete / 5 // 5 days in a working week
 
-    displayResults(totalGLH);
+    let projectedEndDate = getProjectedEndDate(totalDaysToComplete);
+
+    displayResults(totalGLH, totalDaysToComplete, totalWeeksToComplete, projectedEndDate);
+    styleResults(form, totalGLH, projectedEndDate);
+
+    document.getElementById("resultsRow").classList.remove("d-none");
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+
+function styleResults(form, totalGLH, projectedEndDate) {
+    if (totalGLH < parseInt(form.totalGLHInput.value) - 5) {
+        document.getElementById("totalGLHOutput").style = "color: red";
+    } else if (totalGLH > parseInt(form.totalGLHInput.value) + 5) {
+        document.getElementById("totalGLHOutput").style = "color: orange";
+    } else {
+        document.getElementById("totalGLHOutput").style = "color: green";
+    }
+
+    let willMeetDeadline = compareDates(form.deadlineInput.value, projectedEndDate);
+    if (willMeetDeadline) {
+        document.getElementById("endDate").style = "color: red";
+    } else {
+        document.getElementById("endDate").style = "color: green";
+    }
+}
+
+/**
+ * Compare dates and return true if projectedEndDate is earlier
+ * than or equal to the deadline
+ * sources: https://stackoverflow.com/questions/5619202/parsing-a-string-to-a-date-in-javascript
+ * https://stackoverflow.com/questions/492994/compare-two-dates-with-javascript
+ */
+function compareDates(deadlineInput, projectedEndDate) {
+    let parts = deadlineInput.split('-');
+    // Please pay attention to the month (parts[1]); JavaScript counts months from 0:
+    // January - 0, February - 1, etc.
+    let deadlineAsDateObject = new Date(parts[0], parts[1] - 1, parts[2]);
+    return deadlineAsDateObject < projectedEndDate
 }
 
 /**
@@ -71,7 +113,7 @@ function handleSubmit(form) {
  * @param {arr} contentTypes 
  * @param {str} avgVideoLength 
  */
-function calculateGlhPerType(contentTypes, avgVideoLength, contentLevel) {
+function calculateGLHPerType(contentTypes, avgVideoLength, contentLevel) {
 
     let totalGLH = 0;
     for (let i = 0; i < contentTypes.length; i++) {
@@ -101,7 +143,7 @@ function calculateGlhPerType(contentTypes, avgVideoLength, contentLevel) {
 /**
  * Takes decimalTimeString and converts it to hours and minutes
  * Source: https://stackoverflow.com/questions/35460303/how-to-convert-decimal-hour-value-to-hhmmss
-*/
+ */
 function formatDecimalHoursIntoHoursAndMinutes(decimalTimeString) {
     let n = new Date(0, 0);
     n.setMinutes(+decimalTimeString * 60);
@@ -109,7 +151,93 @@ function formatDecimalHoursIntoHoursAndMinutes(decimalTimeString) {
     return result;
 }
 
-function displayResults(totalGLH) {
+function calculateTotalHours(form, totalNumUnits) {
+    let totalHours = 0;
+
+    // Number of hours needed for each phase of project creation
+    let contentCreationHours = {
+        learningOutcomes: 12,
+        assessmentCriteria: 32,
+        syllabusCreation: 40,
+        walkthroughProjectCreation: 160, // 160 for lg
+        contentCreationPerUnit: 11,
+    }
+
+    if (!form.learningOutcomesDone.checked) {
+        // Add hours to create learning outcomes
+        totalHours += contentCreationHours.learningOutcomes;
+    }
+
+    if (!form.assessmentCriteriaDone.checked) {
+        // Add hours to creat assessment criteria
+        totalHours += contentCreationHours.assessmentCriteria;
+    }
+
+    if (!form.syllabusDone.checked) {
+        // Add hours to create syllabus
+        totalHours += contentCreationHours.syllabusCreation;
+    }
+
+    if (!form.wtpSize.checked) {
+        // Add hours to create walkthrough project depending on size
+        let projectSize = form.wtpSize.value;
+        if (projectSize === "sm") {
+            totalHours += (contentCreationHours.walkthroughProjectCreation / 4);
+        } else if (projectSize === "md") {
+            totalHours += (contentCreationHours.walkthroughProjectCreation / 2);
+        } else if (projectSize === "lg") {
+            totalHours += (contentCreationHours.walkthroughProjectCreation);
+        }
+    }
+
+    // Add hours to create content
+    totalHours += totalNumUnits * contentCreationHours.contentCreationPerUnit;
+
+    // Add 20% to total hours as buffer
+    let totalHoursPlus20PercentBuffer = (totalHours / 100) * 120
+    return totalHoursPlus20PercentBuffer
+}
+
+/**
+ * Loop through all unit number inputs and count total
+ * effort for text based units is 50% of video, so this function
+ * calculates .5 per text based unit
+ */
+function getTotalNumUnits(contentTypes) {
+    let total = 0;
+    for (let i = 0; i < contentTypes.length; i++) {
+        let inputValue = document.getElementById(contentTypes[i].inputID).value;
+        if (contentTypes[i].type === "text") {
+            // text units take 50% less time to make than video
+            total += (parseFloat(inputValue) / 2);
+        } else {
+            total += parseFloat(inputValue);
+        }
+    }
+    return total;
+}
+
+/**
+ * Take number of days to create course, add days for the weekends
+ * Calculate date of completion
+ * Source: https://stackoverflow.com/questions/563406/how-to-add-days-to-date
+ */
+function getProjectedEndDate(totalDaysToComplete) {
+    let daysPlusWeekends = (totalDaysToComplete / 5) * 7;
+    let result = new Date();
+    result.setDate(result.getDate() + daysPlusWeekends);
+    return result;
+}
+
+
+/**
+ * Displays calculated results in results section
+ */
+function displayResults(totalGLH, totalDaysToComplete, totalWeeksToComplete, projectedEndDate) {
     let formattedTotalGLH = formatDecimalHoursIntoHoursAndMinutes(totalGLH);
     document.getElementById("totalGLHOutput").innerText = formattedTotalGLH;
+    document.getElementById("totalDaysOutput").innerText = totalDaysToComplete.toFixed(1);
+    document.getElementById("totalWeeksOutput").innerHTML = totalWeeksToComplete.toFixed(1);
+    document.getElementById("endDate").innerHTML = projectedEndDate.toLocaleDateString('en-GB');
+
 }
